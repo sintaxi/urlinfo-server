77409cef63c090e28c323f20380ae70198b17384
Brock Whitten <brock@sintaxi.com>
2019-04-16 23:39:56 -0700

    Adds should as a dependency. Adds initial test for returning lib

---
+++ test/basic.js
@@ -0,0 +1,12 @@
+
+var should  = require("should")
+var urlinfo = require("../")
+
+describe("basic", function(){
+
+  it("should exist", function(done){
+    should.exist(urlinfo)
+    done()
+  })
+
+})
