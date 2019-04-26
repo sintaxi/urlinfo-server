38d8a1ad41ddd8d73a4a60b8162a05a26a19b00b
Brock Whitten <brock@sintaxi.com>
2019-04-26 01:47:45 -0700

    Adds wrappers for LRU caches

--- src/stores/disk.js
+++ src/stores/disk.js
@@ -2,9 +2,10 @@
 var Thug      = require("thug")
 var levelup   = require('levelup')
 var leveldown = require('leveldown')
+var debug     = require("debug")("urlinfo")
 
 
-module.exports = function(storepath){
+module.exports = function(storepath, lrucount){
 
   var db       = levelup(leveldown(storepath))
   var store    = new Thug({ 
@@ -23,6 +24,7 @@ module.exports = function(storepath){
   })
 
   store.constructor.prototype.write = function(identifier, record, callback){
+    debug("DISK write", identifier)
     this.locals.db.put(identifier, record, function(err){
       if (err) return callback(err)
       return callback(record)
@@ -30,6 +32,7 @@ module.exports = function(storepath){
   }
 
   store.constructor.prototype.read = function(identifier, callback){
+    debug("DISK read", identifier)
     this.locals.db.get(identifier, function (err, value) {
       return callback(value || null)
     })
@@ -37,12 +40,39 @@ module.exports = function(storepath){
   }
 
   store.constructor.prototype.remove = function(identifier, record, callback){
+    debug("DISK remove", identifier)
     this.locals.db.del(identifier, function (err) {
       if (err) return callback(err)
       return callback(null)
     })
   }
 
-  return store
+  // No LRU we return the store
+  if (!lrucount) {
+    debug("DISK no LRU", lrucount)
+    return store
+  }
+
+  // if LRU we wrap the methods
+  debug("DISK with LRU", lrucount)
+  return {
+    get: function(id, cb){
+      store.get(id, function(record){
+        return cb(record)
+      })
+    },
+
+    set: function(id, rec, cb){
+      store.set(id, rec, function(errors, record){
+        return cb(errors, record)
+      })
+    },
+
+    del: function(id, cb){
+      store.del(id, function(errors){
+        return cb(errors)
+      })
+    }
+  }
 
 }
--- src/stores/proxy.js
+++ src/stores/proxy.js
@@ -2,12 +2,14 @@
 var Thug        = require("thug")
 var superagent  = require("superagent")
 var path        = require("path")
+var debug     = require("debug")("urlinfo")
 
-module.exports = function(proxydomain){
+module.exports = function(proxydomain, lrucount){
 
   var store = new Thug()
 
   store.constructor.prototype.write = function(identifier, record, callback){
+    debug("PROXY write", proxydomain, identifier)
     superagent
       .put(path.join(proxydomain, identifier))
       .send(record)
@@ -19,6 +21,7 @@ module.exports = function(proxydomain){
   }
 
   store.constructor.prototype.read = function(identifier, callback){
+    debug("PROXY read", proxydomain, identifier)
     superagent
       .get(path.join(proxydomain, identifier))
       .set('accept', 'json')
@@ -29,6 +32,7 @@ module.exports = function(proxydomain){
   }
 
   store.constructor.prototype.remove = function(identifier, record, callback){
+    debug("PROXY remove", proxydomain, identifier)
     superagent
       .del(path.join(proxydomain, identifier))
       .send(record)
@@ -39,6 +43,33 @@ module.exports = function(proxydomain){
       })
   }
 
-  return store
+  // No LRU we return the store
+  // if (!lrucount) {
+  //   debug("PROXY no LRU", lrucount)
+  //   return store
+  // }
+
+  // if LRU we wrap the methods
+  debug("PROXY with LRU", lrucount)
+  return {
+    get: function(id, cb){
+      store.get(id, function(record){
+        return cb(record)
+      })
+    },
+
+    set: function(id, rec, cb){
+      store.set(id, rec, function(errors, record){
+        return cb(errors, record)
+      })
+    },
+
+    del: function(id, cb){
+      store.del(id, function(errors){
+        return cb(errors)
+      })
+    }
+  }
+  
 
 }
