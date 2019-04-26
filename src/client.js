2cb634d98392b52ad841668072a68c4a8544d527
Brock Whitten <brock@sintaxi.com>
2019-04-26 00:45:24 -0700

    Updates bin file with better implementations.

--- src/client.js
+++ src/client.js
@@ -1,24 +1,39 @@
 
-var Thug = require("thug")
-var records = {} // temporary memory store
+var Thug       = require("thug")
+var proxyStore = require("./store-proxy")
+var diskStore  = require("./store-disk")
+var records    = {}
 
 module.exports = function(config){
+  config = config || {}
+  var store;
 
-  var store    = new Thug()
-  store.config = config || {}
+  if (config.hasOwnProperty("proxy")){
+    //console.log("Proxying records to", config.proxy)
+    store = proxyStore(config.proxy)
 
-  store.constructor.prototype.write = function(identifier, record, callback){
-    records[identifier] = record
-    return callback(record)
-  }
+  } else if (config.hasOwnProperty("disk")){
+    //console.log("Storing records on disk", config.disk)
+    store = diskStore(config.disk)
 
-  store.constructor.prototype.read = function(identifier, callback){
-    return callback(records[identifier])
-  }
+  } else {
+    //console.log("Storing records in memory")
+    
+    store = new Thug()
 
-  store.constructor.prototype.remove = function(identifier, record, callback){
-    delete records[identifier]
-    return callback(null)
+    store.constructor.prototype.write = function(identifier, record, callback){
+      records[identifier] = record
+      return callback(record)
+    }
+
+    store.constructor.prototype.read = function(identifier, callback){
+      return callback(records[identifier])
+    }
+
+    store.constructor.prototype.remove = function(identifier, record, callback){
+      delete records[identifier]
+      return callback(null)
+    }
   }
 
   return store
