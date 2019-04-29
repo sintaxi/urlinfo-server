638fd6d21b8ebbd241c0028ad1e9b5f9e580ab20
Brock Whitten <brock@sintaxi.com>
2019-04-29 04:26:12 -0700

    Makes http server more graceful

--- src/server.js
+++ src/server.js
@@ -7,30 +7,35 @@ module.exports = function(store){
   return http.createServer(function(req, rsp){
     //debug("API", req.method, req.url)
 
-    if (req.method == "PUT"){
+    if (["PUT", "POST"].indexOf(req.method) !== -1){
       
       var body = ""
       req.on('data', function(chunk){ body += chunk })
 
       req.on('end', function(){
         req.body = body.toString()
-        store.set(req.url, JSON.parse(req.body), function(errors, record){
-          if (errors) {
-            rsp.writeHead(401)
-            rsp.end()
-          } else {
-            rsp.writeHead(201, { 'Content-Type': 'application/json' })
-            rsp.end(JSON.stringify(record))
-          }
-        })
+        try{
+          var obj = JSON.parse(req.body)
+          store.set(req.url, obj, function(errors, record){
+            if (errors) {
+              rsp.writeHead(400)
+              rsp.end()
+            } else {
+              rsp.writeHead(201, { 'Content-Type': 'application/json' })
+              rsp.end(JSON.stringify(record) + "\n")
+            }
+          })
+        }catch(e){
+          rsp.writeHead(400)
+          rsp.end("Bad Request" + "\n")
+        }
       })
-
       
     } else if(req.method == "GET"){
       store.get(req.url, function(record){
         if (record) {
           rsp.writeHead(200, { 'Content-Type': 'application/json' })
-          rsp.end(JSON.stringify(record))
+          rsp.end(JSON.stringify(record) + "\n")
         } else {
           rsp.writeHead(404)
           rsp.end()
@@ -40,7 +45,7 @@ module.exports = function(store){
     } else if(req.method == "DELETE"){
       store.del(req.url, function(errors){
         if (errors) {
-          rsp.writeHead(410, { 'Content-Type': 'application/json' })
+          rsp.writeHead(400, { 'Content-Type': 'application/json' })
           rsp.end(JSON.stringify(errors))
         } else {
           rsp.writeHead(204)
